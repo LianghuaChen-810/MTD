0c3f484ebee27a2c65902030045d759c66eb2a7b
Animesh Sharma <animeshsharma1996@gmail.com>
2020-10-06 17:36:07 +0100

    Importing UI Buttons

--- MatchTowerDefence/Assets/_Scripts/Core/GameStates/GameStateController.cs
+++ MatchTowerDefence/Assets/_Scripts/Core/GameStates/GameStateController.cs
@@ -2,16 +2,9 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameStateController : MonoBehaviour
+namespace GameCore.GameStates
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
+    public class GameStateController : GameCore.System.Automaton
     {
         
     }
